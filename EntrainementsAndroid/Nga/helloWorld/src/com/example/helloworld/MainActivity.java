53d4301f9a14a826564296765180078a5f01f184
Regis-Gianas <yrg@pps.univ-paris-diderot.fr>
2014-02-27 17:46:35 +0100

    Reunion du 27/02.

--- EntrainementsAndroid/Nga/helloWorld/src/com/example/helloworld/MainActivity.java
+++ EntrainementsAndroid/Nga/helloWorld/src/com/example/helloworld/MainActivity.java
@@ -7,20 +7,20 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		TextView tv = new TextView(this);
-		tv.setText("This is my first Android application!");
-		setContentView(tv);
-	}
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+                TextView tv = new TextView(this);
+                tv.setText("This is my first Android application!");
+                setContentView(tv);
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.main, menu);
+                return true;
+        }
 
 }
