7ad67f9d396827654d9314e12d582058626b618d
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-04-10 17:05:07 +0200

    webservice heroku gestion magasin

---
+++ codeSource/Webservice/WSC/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WSC::Application.config.secret_key_base = 'f3c29b1f4be6b57fc8e8ffde6986ae0fbf013428b70f22eb8f13475077a14a7572a2c9eedeafb02eec0aebc1aa5007edfceb4c719f07e7cbb1431691b8544ed8'
---
+++ codeSource/Webservice/WSC/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WSC::Application.config.session_store :cookie_store, key: '_WSC_session'
