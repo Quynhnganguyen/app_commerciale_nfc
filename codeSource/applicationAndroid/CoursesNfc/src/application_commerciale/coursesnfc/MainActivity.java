5146a0c2e6c2ab4d68fcc513e206dfda80d65ad8
THIMON cynthia <thimon@informatique.univ-paris-diderot.fr>
2014-04-27 20:08:25 +0200

    Création de la page d'inscription + modifs de certaines activités

--- codeSource/applicationAndroid/CoursesNfc/src/application_commerciale/coursesnfc/MainActivity.java
+++ codeSource/applicationAndroid/CoursesNfc/src/application_commerciale/coursesnfc/MainActivity.java
@@ -2,17 +2,21 @@ package application_commerciale.coursesnfc;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	EditText nomUtilisateur;
 	EditText motDePasse;
 	Button boutonConnexion;
+	TextView inscription;
+	Intent intent;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,12 +34,26 @@ public class MainActivity extends Activity {
 				// TODO Auto-generated method stub
 				if (v.getId() == R.id.boutonConnexion) {
 					Toast.makeText(MainActivity.this, "Connexion ",Toast.LENGTH_LONG).show();
-					Intent intent = new Intent(MainActivity.this,AccueilActivity.class);
+					 intent = new Intent(MainActivity.this,AccueilActivity.class);
 					startActivity(intent);
 				}
 			}
 
 		});
+		inscription  = (TextView) findViewById (R.id.inscription);
+		inscription.setText("Pas encore inscrit? Inscrivez-vous de suite!");
+		inscription.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				inscription.setTextColor(Color.WHITE);
+				intent = new Intent(MainActivity.this,InscriptionActivity.class);
+				startActivity(intent);
+			}
+			
+		});
+		
 		
 	}
 	
