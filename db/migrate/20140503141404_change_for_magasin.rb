975352460ea25a06ee6cc90f5cbad43d1a402cd3
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-05-03 18:46:40 +0200

    change database

--- db/migrate/20140503141404_change_for_magasin.rb
+++
@@ -1,5 +0,0 @@
-class ChangeForMagasin < ActiveRecord::Migration
-  def change
-  	drop_table :admin_magasins
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -16,25 +16,6 @@ ActiveRecord::Schema.define(version: 20140503141845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admin_magasins", force: true do |t|
-    t.string   "name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admin_magasins", ["email"], name: "index_admin_magasins_on_email", unique: true, using: :btree
-  add_index "admin_magasins", ["reset_password_token"], name: "index_admin_magasins_on_reset_password_token", unique: true, using: :btree
-
   create_table "admins", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
