dbea0ae043e9ff733ee22f77442bd444002e2ac5
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-05-05 01:14:09 +0200

    for db migrate on heroku

--- db/migrate/20140504225327_franchiseto_magasins.rb
+++
@@ -1,5 +0,0 @@
-class FranchisetoMagasins < ActiveRecord::Migration
-  def change
-  	remove_column :franchises, :magasin_id
-  end
-end
--- db/migrate/20140504225828_add_franchise_ref_to_magasins.rb
+++ db/migrate/20140504225828_add_franchise_ref_to_magasins.rb
@@ -1,6 +1,5 @@
 class AddFranchiseRefToMagasins < ActiveRecord::Migration
   def change
     add_reference :magasins, :franchise, index: true
-    remove_column :magasins, :franshise_id
   end
 end
