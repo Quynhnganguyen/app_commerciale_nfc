eec0ea0d4cae5fcab53d44a4b9a2aca81be79a00
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-05-17 01:41:08 +0200

    for test with new racine

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -31,10 +31,9 @@ class Api::ClientsController < ApplicationController
     render json: results
   end
 
-  def liste_produits
+  def liste_produits #consulte les produit par magasin
     @produits = Produit.where(magasin_id: params[:magasin_id])
     results = @produits.map {|p| {
-             
                   :produit_id => p.id,
                   :produit_nom => p.nom_produit, 
                   :nombre => p.nombre,
@@ -46,5 +45,24 @@ class Api::ClientsController < ApplicationController
   end
 
   def liste_sources
+    @sources = Source.where(magasin_id: params[:magasin_id])
+    results = @sourses.map {|p| {
+                  :source_id => p.id,
+                  :source => p.pays, 
+                  :magasin => p.magasin.nom_magasin}}
+    render json: results
+  end
+
+  def liste_types
+    @types = TypeDeProduit.where(magasin_id: params[:magasin_id])
+    results = @types.map {|p| {
+                  :type_id => p.id,
+                  :type => p.type_produit, 
+                  :magasin => p.magasin.nom_magasin}}
+    render json: results
+  end
+
+  def getProduits
+    # on peut consulter les produit par source/type/prix/
   end
 end
