4fe85980861af36b42fd3c5e819eacc36be57e08
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-05-16 17:13:21 +0200

    for test communication

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # include TokenAuthenticatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
